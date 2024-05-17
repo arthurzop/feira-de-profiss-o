b1619ab6f6121e0cdae941119de01e3c4d0a88f0
arthurzop <medeirosartur48@gmail.com>
2024-05-17 20:32:44 -0300

    framer motion delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 import "./App.css";
 
 export default function App() {
   const location = useLocation()
   return (
     <>
-      <AnimatePresence>
         <Outlet location={location} key={location.pathname} />
-      </AnimatePresence>
     </>
   );
 }
