e359e6c2c27e0d2866b09efd856c6bbdec5c4d64
aarthurzop <medeirosartur48@gmail.com>
2024-05-22 19:35:45 -0300

    config .env no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import "./App.css";
+import dotenv from "dotenv";
 
+dotenv.config();
 export default function App() {
-  const location = useLocation()
+  const location = useLocation();
+
   return (
     <>
-        <Outlet location={location} key={location.pathname} />
+      <Outlet location={location} key={location.pathname} />
     </>
   );
 }
